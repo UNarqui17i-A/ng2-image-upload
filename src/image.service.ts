c4d74098a051851e1e7b17f90a8b02ddf5b1b274
Rodrigo Espinel <riespinelv@unal.edu.co>
2017-04-16 13:05:11 -0500

    test

--- src/image.service.ts
+++ src/image.service.ts
@@ -53,4 +53,5 @@ export class ImageService {
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     console.error(errMsg); // log to console instead
     return Observable.throw(errMsg);
+  }
 }
